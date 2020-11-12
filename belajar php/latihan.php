7cb24b5c0945cb98a0e79e9527e275dc4c926bc1
sarahnindya <69177714+sarahnindya@users.noreply.github.com>
2020-11-12 23:03:29 +0700

    Add files via upload

---
+++ belajar php/latihan.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <fieldset>
+        <label>Input Data</label>
+        <form method="GET" action="proses1.php">
+            <p>Nama : <input type="text" name="nama"></p>
+            <p>Email : <input type="email" name="email"></p>
+            <p><input type="submit" value="Proses Data" name="submit"></p>
+        </form>
+    </fieldset>
+</body>
+</html>
